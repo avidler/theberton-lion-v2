4c96ad112115023eb7793dffdf94ba3b3f7b07b8
avidler <a.vidler@live.co.uk>
2020-05-28 16:17:33 +0100

    siema

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,8 @@ import { Link, useStaticQuery, graphql } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import Siema from 'siema';
+new Siema();
 
 const IndexPage = () => (
   <Layout>
